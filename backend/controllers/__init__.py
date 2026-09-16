4b8cdc7c73245b35727c2b0da8626a285120b487
Paolo <paolodev.fullstack@gmail.com>
2026-09-15 21:24:51 -0300

    Delete backend directory

--- backend/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .index_controller import index_bp
-from .loja_controller import loja_bp
-from .pedido_controller import pedido_bp
-from .produto_controller import produto_bp
-from .user_controller import user_bp
-
-
-__all__ = ["index_bp","loja_bp","pedido_bp","user_bp", "produto_bp"]
